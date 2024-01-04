2811bf86b1631a4cad3aa187e34ecf58b3ff6bc9
ppdoli123 <ppdoli123@naver.com>
2024-01-04 16:21:46 +0900

    feat(NaverDTO): access 변수 추가

--- src/main/java/com/app/sociallogin/naver/dto/NaverDTO.java
+++ src/main/java/com/app/sociallogin/naver/dto/NaverDTO.java
@@ -1,5 +1,6 @@
 package com.app.sociallogin.naver.dto;
 
+import jdk.jshell.Snippet;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,6 +11,9 @@ public class NaverDTO {
     private String id;
     private String email;
     private String name;
+    private String access;
+    public String getAccess(){ return access;}
+    public String getId() {return id; }
 
     public String getEmail() {
         return email;
