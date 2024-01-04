a77738da12c9c4e0490008cba256a0a39df108b4
ppdoli123 <ppdoli123@naver.com>
2024-01-04 16:19:14 +0900

    refactor(ProfileController): 프로필 예외사항 구현

--- src/main/java/com/app/profile/ProfileController.java
+++ src/main/java/com/app/profile/ProfileController.java
@@ -1,26 +1,32 @@
 package com.app.profile;
 
-import com.app.entitiy.User;
 import com.app.repository.UserRepository;
+import com.app.sociallogin.common.MsgEntity;
 import com.app.sociallogin.naver.service.NaverService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class ProfileController {
-
+    private final NaverService naverservice;
     private final UserRepository userRepository;
-    public ProfileController(UserRepository userRepository) {
+    public ProfileController(NaverService naverservice, UserRepository userRepository) {
+        this.naverservice = naverservice;
         this.userRepository = userRepository;
     }
     @GetMapping("/profile")
-    public String profile(HttpSession session, Model model) {
-        String email = (String)session.getAttribute("email");
-        User user = userRepository.findByUserid(email);
-        model.addAttribute("user", user);
-        return "profile";
+    public ResponseEntity<MsgEntity> profile(HttpSession session) {
+    try{
+        return ResponseEntity.ok()
+                .body(new MsgEntity("Success", session.getAttribute("email")));
+
+    } catch (Exception e) {
+        e.printStackTrace();
+        return ResponseEntity.status(500)
+                .body(new MsgEntity("Error", null)); // 실패 시 에러 메시지 반환
+    }
     }
 }
